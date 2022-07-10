3b79a09a4232d565705e2697d6904ff954530e20
renan <renan@studiocherie.net>
2022-07-10 18:19:38 +0200

    add category

--- src/main/java/com/lepus/rentalequipment/entities/Product.java
+++ src/main/java/com/lepus/rentalequipment/entities/Product.java
@@ -2,10 +2,12 @@ package com.lepus.rentalequipment.entities;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.Hibernate;
 
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Getter
 @Setter
@@ -28,6 +30,10 @@ public class Product {
     @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Item> items = new ArrayList<>();
 
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
     public void addItem(Item item) {
         items.add(item);
         item.setProduct(this);
@@ -38,4 +44,16 @@ public class Product {
         item.setProduct(null);
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Product product = (Product) o;
+        return id != null && Objects.equals(id, product.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
