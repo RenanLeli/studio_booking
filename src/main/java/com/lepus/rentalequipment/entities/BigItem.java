636a673f2e3f475a52d2d2f3cb9621f7c75f4d2b
Yuan <yuan.zong.man@gmail.com>
2022-07-10 17:45:49 +0200

    add entity attribute for BigItem automatically

--- src/main/java/com/lepus/rentalequipment/entities/BigItem.java
+++ src/main/java/com/lepus/rentalequipment/entities/BigItem.java
@@ -3,12 +3,14 @@ package com.lepus.rentalequipment.entities;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 
 @Getter
 @Setter
 @Entity
 public class BigItem extends Item {
+    @Column(name = "barcode")
+    private String barcode;
 
-    public String barcode;
 }
